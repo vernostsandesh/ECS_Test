c7d975bcb0f374ed3dd91558e111bc026a52a386
vernostsandesh <44458753+vernostsandesh@users.noreply.github.com>
2021-12-05 09:52:43 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const app = express{};
+
+app.get("/",(req,res) => res.send("Hello World !")};
+
+app.get ("/health", (req, res) => {
+    res.status(200);
+    res.send ("healthy");
+});
+
+app.listen 3000,() => {
+console.log ("App listining on port 3000");
+};
