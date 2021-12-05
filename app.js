60d831c90e598bf4aed96fd43273524aeecd16aa
vernostsandesh <44458753+vernostsandesh@users.noreply.github.com>
2021-12-05 17:19:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Youtube!"));
+app.get("/", (req, res) => res.send("Hello Sandesh, This is First Fargate Project!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
